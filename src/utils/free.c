10719f067d6424fc263ce33c5051a03e59ef6419
alex <aherlaud@student.42.fr>
2025-07-11 00:07:00 +0200

    norm and cleanup for free_all, expansion, join and main for end of project tests

--- src/utils/free.c
+++ src/utils/free.c
@@ -3,31 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lflayeux <lflayeux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 22:29:14 by pandemonium       #+#    #+#             */
-/*   Updated: 2025/07/01 11:47:01 by lflayeux         ###   ########.fr       */
+/*   Updated: 2025/07/10 23:58:58 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	free_all(t_shell *shell)
+void free_rest(t_shell *shell)
 {
-	(void)shell;
-	close_fd(shell, 2, 0);
-	if (shell->input)
-		free(shell->input);
-	if (shell->signals)
-		free(shell->signals);
-	if (shell->tok)
-		ft_lstclear_tok(shell->tok);
-	if (shell->exec)
-		ft_lstclear_exec(shell->exec);
-	if (shell->env)
-		ft_free_tab((void **)shell->env);
-	if (shell->secret)
-		ft_free_tab((void **)shell->secret);
 	if (PIPEX)
 	{
 		if (PIPEX->child_tab)
@@ -41,4 +27,23 @@ void	free_all(t_shell *shell)
 		free(shell->pid);
 	if (shell)
 		free(shell);
+}
+
+void	free_all(t_shell *shell)
+{
+	// (void)shell;
+	close_fd(shell, 2, 0);
+	if (shell->input)
+		free(shell->input);
+	if (shell->signals)
+		free(shell->signals);
+	if (shell->tok)
+		ft_lstclear_tok(shell->tok);
+	if (shell->exec)
+		ft_lstclear_exec(shell->exec);
+	if (shell->env)
+		ft_free_tab((void **)shell->env);
+	if (shell->secret)
+		ft_free_tab((void **)shell->secret);
+	free_rest(shell);
 }
