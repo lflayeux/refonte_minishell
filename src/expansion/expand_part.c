41168eb068f7e10f27cdb7e8fbb2bc1f6acfb122
Lorenzo Flayeux <lflayeux@f6r4s6.clusters.42paris.fr>
2025-07-13 18:40:20 +0200

    fix : expand with dollar

--- src/expansion/expand_part.c
+++ src/expansion/expand_part.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand_part.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aherlaud <aherlaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lflayeux <lflayeux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 14:51:37 by lflayeux          #+#    #+#             */
-/*   Updated: 2025/07/13 17:12:56 by aherlaud         ###   ########.fr       */
+/*   Updated: 2025/07/13 18:39:21 by lflayeux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,11 +45,7 @@ void	quotes_expand(t_expand *expand, char sep, t_shell *shell)
 	expand->i++;
 	start = expand->i;
 	while (expand->word[expand->i] != sep && expand->word[expand->i])
-	{
-		if (expand->word[expand->i] == '$' && sep == '"')
-			break ;
 		expand->i++;
-	}
 	new_len = ft_strlen(expand->new) + (expand->i - start) + 1;
 	expand->new = ft_realloc(expand->new, new_len);
 	if (expand->new == NULL)
@@ -57,11 +53,6 @@ void	quotes_expand(t_expand *expand, char sep, t_shell *shell)
 	ft_memcpy(expand->new + ft_strlen(expand->new), &(expand->word[start]),
 		expand->i - start);
 	expand->new[new_len - 1] = '\0';
-	if (expand->word[expand->i] == '$' && sep == '"')
-	{
-		dollar_expand(expand, shell);
-		quotes_expand(expand, sep, shell);
-	}
 	expand->quotes = 0;
 	expand->i++;
 }
